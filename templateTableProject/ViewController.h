a4d0ff242af5c5075f95226938ded137b885896c
Fabian Buentello <fabian.cardenas93@gmail.com>
2015-02-12 15:22:21 -0600

    Initial Commit

---
+++ templateTableProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  templateTableProject
+//
+//  Created by Fabian Buentello on 2/12/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ templateTableProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  templateTableProject
+//
+//  Created by Fabian Buentello on 2/12/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
